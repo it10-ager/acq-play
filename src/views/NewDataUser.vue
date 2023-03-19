9d7106f729ae3303492b8944773193da90bd4ad3
German Trushkov <germantrushkov@gmail.com>
2023-03-19 23:50:12 +0200

    add some changes

--- src/views/NewDataUser.vue
+++ src/views/NewDataUser.vue
@@ -8,15 +8,22 @@
 		:class="{ 'is-invalid': nameInvalid, 'is-valid': nameValid }" />
 		
 		<!--Gender-->
-		<div class="block-text">
+		<div class="block-text gender" @click="showGender = !showGender">
 			<div class="placeholder-txt">
 				<span>{{ gender }}</span>
 			</div>
+
 			<div class="arrow">
 				<span></span>
 				<span></span>
 			</div>
-		</div>
+
+			<ul v-if="showGender" class="list">
+				<li v-for="(genderOption, index) in genderOptions" :key="index">
+					<span @click="selectGender(genderOption)">{{ genderOption }}</span>
+				</li>
+			</ul>
+  		</div>
 
 		<!--Date birthday-->
 		<input class="input-text" type="text" :placeholder="birthValue" v-model="birth"
@@ -31,15 +38,22 @@
 		:class="{ 'is-invalid': companyInvalid, 'is-valid': companyValid }" />
 
 		<!--Busy-->
-		<div class="block-text">
+		<div class="block-text busy" @click="showBusy = !showBusy">
 			<div class="placeholder-txt">
 				<span>{{ busy }}</span>
 			</div>
+
 			<div class="arrow">
 				<span></span>
 				<span></span>
 			</div>
-		</div>
+
+			<ul v-if="showBusy" class="list">
+				<li v-for="(busyOption, index) in busyOptions" :key="index">
+					<span @click="selectBusy(busyOption)">{{ busyOption }}</span>
+				</li>
+			</ul>
+  		</div>
 
 		<!--Target-->
 		<div class="block-text">
@@ -80,7 +94,13 @@
 				nameValid: false,
 
 				gender: 'Выберите пол',
+				genderOptions: ['Мужской', 'Женский'],
+     			showGender: false,
+
 				busy: 'Выберите род занятий',
+				busyOptions: ['Работаю', 'Учусь', 'Ничего не делаю'],
+				showBusy: false,
+
 				target: 'Выберите цель в проекте',
 
 				birth: '',
@@ -106,7 +126,31 @@
 		},
 
 		methods: {
-			
+			selectGender(genderOption) {
+				this.gender = genderOption;
+				this.showGender = false;
+
+				var arrow = document.querySelectorAll('.gender .arrow > span');
+				arrow.forEach(function(arrow) {
+  					arrow.style.backgroundColor = '#3cb371';
+   		 		});
+
+				var newColor = document.querySelector('.gender .placeholder-txt > span')
+				newColor.style.color = '#000';
+			},
+
+			selectBusy(busyOption) {
+				this.busy = busyOption;
+				this.showBusy = false;
+
+				var arrow = document.querySelectorAll('.busy .arrow > span');
+				arrow.forEach(function(arrow) {
+  					arrow.style.backgroundColor = '#3cb371';
+   		 		});
+
+				var newColor = document.querySelector('.busy .placeholder-txt > span')
+				newColor.style.color = '#000';
+			},
 		},
 	}
 </script>
@@ -118,9 +162,10 @@
 	.new-data {
 		@extend %mainWrapper;
 		font-size: 18px;
+
 		.title {@extend %titleFont;}
-	
-		.input-text, .block-text{
+
+		.input-text, .block-text {
 			border: 0;
 			outline: none;
 			padding: 15px 30px;
@@ -130,47 +175,83 @@
 			margin-bottom: 20px;
 
 			&.is-invalid {
-				&::placeholder {
-					color: $errorColor;
-				}
+				&::placeholder {color: $errorColor;}
 			}
+
 			&::placeholder {color: $placeholderColor;}
 		}
 
-		.block-text{
+		.block-text {
 			display: flex;
 			justify-content: space-between;
 			align-items: center;
 			background: #fff;
+			position: relative;
+			cursor: pointer;
 
-			.placeholder-txt{
+			.placeholder-txt {
 				color: $placeholderColor;
 				width: 100%;
+				position: relative;
+				z-index: 7;
 			}
 
-			.arrow{
+			.arrow {
 				display: flex;
 				width: 10px;
-				align-items: center;
 				justify-content: center;
+				z-index: 12;
 
-				>span{
+				>span {
 					height: 10px;
 					width: 2px;
 					background-color: $placeholderColor;
 
-					&:first-of-type{
+					&:first-of-type {
 						transform: rotate(-45deg);
 						margin-right: 4px;
 					}
 
-					&:last-of-type{transform: rotate(45deg);}
+					&:last-of-type {transform: rotate(45deg);}
 				}
+			}
+
+			.list {
+				position: absolute;
+				top: 55%;
+				left: 0;
+				height: 105px;
+				width: 100%;
+				padding: 0;
+				list-style: none;
+				background-color: #fff;
+				box-shadow: 0 2px 2px rgba(0, 0, 0, 0.1);
+				border-radius: 0 0 29px 29px;
+				z-index: 5;
+				display: flex;
+				flex-direction: column;
+				justify-content: flex-end;
+				overflow: hidden;
+				cursor: default;
+
+				li {
+					padding: 8px 30px;
+					&:last-of-type{padding-bottom: 12px;}
+					
+					>span{
+						cursor: pointer;
+						&:hover {color: $bgMain;}
+					}
+				}
+			}
+
+			&.busy{
+				.list{height: 150px;}
 			}
 		}
 
 		.join {
-			.next-block{
+			.next-block {
 				display: flex;
 				justify-content: space-between;
 				align-items: center;
@@ -186,5 +267,4 @@
 			}
 		}
 	}
-  
 </style>
