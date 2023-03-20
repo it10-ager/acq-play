5f3ca4b4cc1f18de07bb19e388cd24147a24348f
German Trushkov <germantrushkov@gmail.com>
2023-03-20 22:48:32 +0200

    add around block

--- src/views/Around.vue
+++ src/views/Around.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="user-container">
 		
-		<div class="users-photos-first">
+		<div class="users-photos first">
 			<div class="another-photos">
 				<div>
 					<img src="../assets/img/background/around_user.png" alt="user">
@@ -12,11 +12,66 @@
 			</div>
 			<div class="main-photo">
 				<img src="../assets/img/background/around_user.png" alt="user">
+				<div class="user-info">
+					<div class="info-txt">
+						<p>Моё имя, 28</p>
+						<p>Работаю в Корпорация</p>
+						<p>Москва, знакомлюсь</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<div class="users-photos second">
+			<div class="another-photos">
+				<div>
+					<img src="../assets/img/background/around_timer_user.png" alt="user">
+					<div class="timer">
+						<p>23:59</p>
+						<p>до поптыки</p>
+					</div>
+				</div>
+				<div>
+					<img src="../assets/img/background/around_user.png" alt="user">
+				</div>
+				<div>
+					<img src="../assets/img/background/around_user.png" alt="user">
+				</div>
+			</div>
+
+			<div class="another-photos">
+				<div>
+					<img src="../assets/img/background/around_user.png" alt="user">
+				</div>
+				<div>
+					<img src="../assets/img/background/around_user.png" alt="user">
+				</div>
+				<div>
+					<img src="../assets/img/background/around_user.png" alt="user">
+				</div>
+			</div>
+
+			<div class="another-photos">
+				<div>
+					<img src="../assets/img/background/around_mess_user.png" alt="user">
+					<div class="message">
+						<div class="count">
+							<img src="../assets/img/elements/messages.png" alt="message">
+						</div>
+						<span>Переписка</span>
+					</div>
+				</div>
+				<div>
+					<img src="../assets/img/background/around_user.png" alt="user">
+				</div>
+				<div>
+					<img src="../assets/img/background/around_user.png" alt="user">
+				</div>
 			</div>
 		</div>
 
 		<!--Main block to next page-->
-		<b-row class="search d-flex justify-content-center my-2">
+		<b-row class="search d-flex justify-content-center my-2" @click="showNewPhotos">
 			<b-col>
 				<div class="next-block">
 					<span class="w-100 text-center">Расширенный поиск</span>
@@ -34,7 +89,6 @@
 
 	export default {
 		name: 'Around',
-
 		components: {
 			MenuBlock,
 		},
@@ -50,7 +104,12 @@
 		},
 
 		methods: {
-			
+			showNewPhotos(){
+				var second = document.querySelector('.second');
+				second.style.display = 'flex';
+				second.style.marginBottom = '80px';
+				document.querySelector('.next-block').style.display = 'none';
+			}
 		}
 	};
 </script>
@@ -59,35 +118,166 @@
 	@import '../assets/extend';
 	@import '../assets/vars';
 
-	.user-container {
+	.user-container{
 		@extend %mainWrapper;
 		justify-content: flex-start;
+		max-width: 425px;
 		
-		.users-photos-first{
+		.users-photos{
 			display: flex;
 			justify-content: space-between;
-			margin-bottom: 40px;
+			margin-bottom: 20px;
 
-			.another-photos{
-				display: flex;
-				flex-direction: column;
-				margin-right: 10px;
-				justify-content: space-between;
-				
-				>div{
-					width: 91px;
-					>img{
-						width: inherit;
-						border-radius: 8px;
+			&.second{
+				display: none;
+				margin-bottom: 40px;
+				.another-photos:last-of-type{margin-right: 0px;}
+				.another-photos{
+					>div{
+						width: 100%;
+					}
+
+					&:nth-child(1){
+						>div:nth-child(1){
+							position: relative;
+							.timer{
+								display: flex;
+								flex-direction: column;
+								align-items: center;
+								justify-content: center;
+								position: absolute;
+								bottom: 10px;
+								left: 0;
+								right: 0;
+								color: $fontColor;
+								
+								>p{
+									margin: 0;
+									font-size: 12px;
+									&:first-of-type{font-size: 22px;}
+									@media screen and (min-width: 410px){
+										font-size: 16px;
+										&:first-of-type{font-size: 28px;}
+									}
+								}
+							}
+						}
+					}
+
+					&:nth-child(3){
+						>div:nth-child(1){
+							position: relative;
+							.message{
+								display: flex;
+								flex-direction: column;
+								align-items: center;
+								justify-content: center;
+								position: absolute;
+								bottom: 10px;
+								left: 0;
+								right: 0;
+								color: $fontColor;
+								
+								.count{
+									>img{
+										margin-bottom: 5px;
+										width: 50px;
+										@media screen and (min-width: 410px){
+											width: 60px;
+										}
+									}
+								}
+
+								>span{
+									margin: 0;
+									font-size: 12px;
+									@media screen and (min-width: 410px){
+										font-size: 16px;
+									}
+								}
+							}
+						}
 					}
 				}
 			}
 
+			.another-photos{
+				display: flex;
+				flex-direction: column;
+				justify-content: space-between;
+				margin-right: 10px;
+				
+				>div{
+					width: 90px;
+					&:not(:last-of-type){margin-bottom: 10px;}
+					>img{
+						width: 100%;
+						border-radius: 8px;
+					}
+					@media screen and (min-width: 340px){width: 95px;}
+					@media screen and (min-width: 350px){width: 100px;}
+					@media screen and (min-width: 360px){width: 102px;}
+					@media screen and (min-width: 370px){width: 105px;}
+					@media screen and (min-width: 390px){width: 110px;}
+					@media screen and (min-width: 410px){width: 120px;}
+				}
+			}
+
 			.main-photo{
-				width: 80%;
+				width: 100%;
+				position: relative;
 				>img{
 					width: inherit;
 					border-radius: 8px;
+				}
+
+				.user-info{
+					background: #fff;
+					border: 1px solid #3cb371;
+					border-radius: 10px;
+					display: flex;
+					align-items: center;
+					box-shadow: 0px 1px 2px 0px rgba(187, 187, 187, 0.72);
+					font-size: 12px;
+					width: 90%;
+					position: absolute;
+					bottom: 10px;
+					left: 0;
+					right: 0;
+					margin: auto;
+					padding: 10px;
+					padding: 7px 15px;
+
+					.info-txt{
+						>p{
+							margin: 0;
+							&:first-of-type{
+								font-weight: bold;
+								font-size: 14px;
+								
+								>sup{
+									font-size: 12px;
+									font-weight: normal;
+									color: $bgMain;
+									cursor: pointer;
+								}
+							}
+						}
+					}
+					
+					@media screen and (min-width: 410px){
+						
+						padding: 10px 20px;
+						.info-txt{
+						margin-right: 30px;
+							>p{
+								font-size: 14px;
+								&:first-of-type{
+									font-size: 16px;
+								}
+							}
+						}
+					}
 				}
 			}
 		}
